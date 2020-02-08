5d6499e6fc55013a0a711e72d75e7c3e6b40f672
Marek <janusz.marek@corp.altegrity.com>
2020-02-08 14:27:45 +0100

    Controllers

---
+++ HomeAutomation/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeAutomation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult Home()
+        {
+            return Ok(new
+            {
+                ApiName = "HomeAutomation",
+                ApiVersion = "1.0.0",
+            });
+        }
+    }
+}
---
+++ HomeAutomation/Controllers/ProducerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeAutomation.Models.Entities;
+using HomeAutomation.Repositories;
+using HomeAutomation.Repositories.Interfaces;
+using HomeAutomation.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeAutomation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private IProducerService producerService;
+
+        public ProducerController(IProducerService producerService)
+        {
+            this.producerService = producerService;
+        }
+
+        [HttpGet()]
+        public async Task<IEnumerable<Producer>> GetListing()
+        {
+            return await producerService.GetAsync();
+        }
+    }
+}
