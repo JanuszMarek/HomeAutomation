b28a8962691e43168644f6aabbffe54caea6d520
Marek <janusz.marek@corp.altegrity.com>
2020-06-22 21:32:04 +0200

    Connection enum

---
+++ HomeAutomation/Migrations/20200622193141_ConnectionEnum.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeAutomation.Migrations
+{
+    public partial class ConnectionEnum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Connection",
+                schema: "HA",
+                table: "Device",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Connection",
+                schema: "HA",
+                table: "Device");
+        }
+    }
+}
---
+++ HomeAutomation/Models/Enums/ConnectionEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace HomeAutomation.Models.Enums
+{
+    public enum ConnectionEnum
+    {
+        [Description("None")]
+        None = 0,
+        [Description("Wi-Fi")]
+        WiFi,
+        [Description("Bluetooth BLE")]
+        BLE,
+        [Description("ZigBee")]
+        ZigBee,
+        [Description("Z-Wave")]
+        ZWave
+    }
+}
